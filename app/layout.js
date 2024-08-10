a61a0afbd97fa683d1514255366607bb5e51be59
nikaliwood <167284895+nikaliwood@users.noreply.github.com>
2024-08-09 20:29:26 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Nikali's Pantry",
+  title: "Pantry Track",
   description: "Created using next.js",
 };
 
